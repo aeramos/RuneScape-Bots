18a7013e3ec51183d9a106415fb9a63134f9d3e9
SuperBotter <SuperBotter@a474b641-a148-44e9-9bf8-48c4f81c8702>
2016-06-28 05:27:45 +0000

    Removed the use of viewport paths to travel large distances (bank to mine and mine to bank). Edited the manifest to make it more like a real bot. Removed usage of stopwatches as the values of them are not shown yet (when I get a GUI they will be shown)

--- src/com/SuperBotter/bots/SuperMiner/SuperMiner.java
+++ src/com/SuperBotter/bots/SuperMiner/SuperMiner.java
@@ -2,19 +2,24 @@ package com.SuperBotter.bots.SuperMiner;
 
 import com.SuperBotter.bots.SuperMiner.tasks.Mine;
 import com.SuperBotter.bots.SuperMiner.tasks.Store;
-import com.runemate.game.api.hybrid.util.StopWatch;
+import com.runemate.game.api.hybrid.location.Area;
+import com.runemate.game.api.hybrid.location.navigation.Path;
+import com.runemate.game.api.hybrid.location.navigation.basic.BresenhamPath;
+import com.runemate.game.api.hybrid.location.navigation.cognizant.RegionPath;
 import com.runemate.game.api.script.framework.task.TaskScript;
 
+//import com.runemate.game.api.hybrid.util.StopWatch;
+
 public class SuperMiner extends TaskScript {
-    private StopWatch stopWatch = new StopWatch();
+    //private StopWatch stopWatch = new StopWatch();
     @Override
     public void onStart(String... args) {
-        stopWatch.start();
+        //stopWatch.start();
         setLoopDelay(300, 750); // in ms (1000ms = 1s)
         add(new Mine(), new Store());
         // get input from player on what to mine
         // used to decide if you add bank or drop
-    }
+    }/*
     @Override
     public void onPause() {
         stopWatch.stop();
@@ -26,5 +31,17 @@ public class SuperMiner extends TaskScript {
     @Override
     public void onStop() {
         stopWatch.stop();
+    }*/
+
+    // run by Mine and Store to go to their different areas
+    public static void goToArea(Area destination) {
+        Path p;
+        p = RegionPath.buildTo(destination.getRandomCoordinate());
+        if (p == null) {
+            p = BresenhamPath.buildTo(destination.getRandomCoordinate());
+        }
+        if(p != null) {
+            p.step();
+        }
     }
 }
